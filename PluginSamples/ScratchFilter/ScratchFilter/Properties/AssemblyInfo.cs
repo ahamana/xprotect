65f6c40799117d11aeb6397da044444f52a872fb
hamana.akihiro <hamana.akihiro@mail.canon>
2020-06-18 10:36:14 +0900

    著作権情報を追加

--- PluginSamples/ScratchFilter/ScratchFilter/Properties/AssemblyInfo.cs
+++ PluginSamples/ScratchFilter/ScratchFilter/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Canon Inc.")]
 [assembly: AssemblyProduct("Scratch Filter")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
