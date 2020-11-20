342b673bbe87ca3b67f04fd85b8891c9e36b53bb
hamana.akihiro <hamana.akihiro@mail.canon>
2020-11-20 11:13:35 +0900

    著作権表記を追加

--- PluginSamples/ScratchFilter/ScratchFilter/Common/CameraManager.cs
+++ PluginSamples/ScratchFilter/ScratchFilter/Common/CameraManager.cs
@@ -1,3 +1,11 @@
+//
+// (C) 2020 Canon Inc.
+//
+// This file contains trade secrets of Canon Inc. No part may be reproduced
+// or transmitted in any form by any means or for any purpose without the
+// express written permission of Canon Inc.
+//
+
 using System.Collections.Generic;
 using VideoOS.Platform;
 
