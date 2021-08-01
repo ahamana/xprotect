ecb0377b782e13632da80cc5cd7b15d61c4aad05
Akihiro Hamana <doarat@gmail.com>
2021-08-02 01:31:07 +0900

    拡張メソッドを追加

---
+++ PluginSamples/ScratchFilter/ScratchFilter/Extensions/JPEGDataExtensions.cs
@@ -0,0 +1,37 @@
+﻿//
+// (C) 2020 Canon Inc.
+//
+// This file contains trade secrets of Canon Inc. No part may be reproduced
+// or transmitted in any form by any means or for any purpose without the
+// express written permission of Canon Inc.
+//
+
+using System.Drawing;
+using System.IO;
+
+using VideoOS.Platform.Data;
+
+namespace ScratchFilter.Extensions
+{
+    /// <summary>
+    /// <see cref="JPEGData" /> の拡張メソッドです。
+    /// </summary>
+    internal static class JPEGDataExtensions
+    {
+        #region Methods
+
+        /// <summary>
+        /// 画像を取得します。
+        /// </summary>
+        /// <param name="jpegData"><see cref="JPEGData" /></param>
+        /// <returns>画像</returns>
+        internal static Bitmap GetBitmap(this JPEGData jpegData)
+        {
+            using var stream = new MemoryStream(jpegData.Bytes);
+
+            return new(stream);
+        }
+
+        #endregion Methods
+    }
+}
